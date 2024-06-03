a6788616ef99823ad45ad5669ccf29d7799a5290
Chandan Suri <csuri21296@gmail.com>
2024-06-03 15:39:03 -0400

    Added exit command functionality and resolved an error.

--- app/main.py
+++ app/main.py
@@ -9,12 +9,13 @@ def main():
         sys.stdout.flush()
 
         command = input()
-        if command not in VALID_COMMANDS:
-            sys.stdout.write(f"{command}: command not found\n")
-            continue
-
         commandParams = command.split(" ")
         commandName, args = commandParams[0], commandParams[1]
+
+        if commandName not in VALID_COMMANDS:
+            sys.stdout.write(f"{commandName}: command not found\n")
+            continue
+
         if commandName == "exit" and args == "0":
             break
 
