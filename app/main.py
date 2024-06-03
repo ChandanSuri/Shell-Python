40c2f672929c4ccf772d8c1fec9cdd2062509728
Chandan Suri <csuri21296@gmail.com>
2024-06-03 15:26:18 -0400

    Pass 1st Stage

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
 import sys
 
-
 def main():
-    print("Logs from your program will appear here!")
-
     sys.stdout.write("$ ")
     sys.stdout.flush()
 
     # Wait for user input
     input()
-
 
 if __name__ == "__main__":
     main()
